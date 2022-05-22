b6bf07ce04a35c0c808deace775259eaf6250dc5
lebrancconvas <lebrancconvas@gmail.com>
2022-05-23 02:48:48 +0700

    💄 Create Homepage UI.

---
+++ client/src/pages/Homepage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Homepage: React.FC = () => {
+	return(
+		<div>
+			<h1 className="flex justify-center items-center text-3xl">Thai Youtuber Election</h1>
+			<br />
+			<h2 className="flex justify-center items-center text-xl">เลือกตั้งผู้ว่า Youtuber ไทย</h2>
+		</div>
+	)
+};
+
+export default Homepage;
