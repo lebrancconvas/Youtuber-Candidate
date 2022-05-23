b985264d27400738aed14362cb9025e166ac8e0c
lebrancconvas <lebrancconvas@gmail.com>
2022-05-23 14:47:13 +0700

    🔨 Create Interface.

--- client/src/lib/youtuber.interface.ts
+++ client/src/lib/youtuber.interface.ts
@@ -1,8 +1,18 @@
+interface ThumbnailData {
+	url: string;
+	width: number;
+	height: number;
+}
+
+interface Thumbnail {
+	medium: ThumbnailData;
+}
+
 interface Snippet {
 	title: string;
 	description: string;
+	thumbnails: Thumbnail;
 }
-
 export interface YoutuberData {
 	id: string;
 	snippet: Snippet;
