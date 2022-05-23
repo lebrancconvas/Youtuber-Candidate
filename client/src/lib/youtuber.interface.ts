e3a0eb49da59f7f83d35b49a846ff1926fd16cbf
lebrancconvas <lebrancconvas@gmail.com>
2022-05-23 20:22:11 +0700

    🔨 Update Interface.

--- client/src/lib/youtuber.interface.ts
+++ client/src/lib/youtuber.interface.ts
@@ -8,12 +8,20 @@ interface Thumbnail {
 	medium: ThumbnailData;
 }
 
+interface Statistic {
+	viewCount: string,
+	subscriberCount: string,
+	hiddenSubscriberCount: boolean,
+	videoCount: string 
+}
+
 interface Snippet {
 	title: string;
 	description: string;
 	thumbnails: Thumbnail;
-}
+} 
 export interface YoutuberData {
 	id: string;
 	snippet: Snippet;
-}
+	statistics: Statistic; 
+} 
