f8bdca261fb6c2fa17f29a19600ed2972cd6bfe6
lebrancconvas <lebrancconvas@gmail.com>
2022-05-23 23:35:47 +0700

    ✨ Create Footer.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,18 +7,24 @@ import About from './pages/About';
 import Trend from './pages/Trend';
 
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/candidate" element={<Candidate />} />
-          <Route path="/trend" element={<Trend />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
+      <div className="flex flex-col h-screen justify-between"> 
+          <Navbar />
+          <div className="mb-auto">
+            <Routes>
+              <Route path="/" element={<Homepage />} />
+              <Route path="/candidate" element={<Candidate />} />
+              <Route path="/trend" element={<Trend />} />
+              <Route path="/about" element={<About />} />
+            </Routes>
+          </div>
+          <Footer />
+        </div>
       </BrowserRouter> 
     </>
   );
---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+	return(
+		<footer className="text-center"> 
+			<h3>Website is created by Poom Yimyuean (lebrancconvas) with React and FastAPI</h3>
+		</footer>
+	)
+};
+
+export default Footer;
