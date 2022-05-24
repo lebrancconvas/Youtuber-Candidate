7ffba474c98a22a4323f0586a817fb2376f9327d
lebrancconvas <lebrancconvas@gmail.com>
2022-05-25 05:47:41 +0700

    💬 Update Content.

--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -4,9 +4,16 @@ import React from 'react';
 
 const Homepage: React.FC = () => {
 	return(
-		<div>
+		<div className="text-center"> 
 			<h1 className="flex justify-center items-center text-3xl mb-3">Thai Youtuber Election</h1>
 			<h2 className="flex justify-center items-center text-xl">เลือกตั้งผู้ว่า Youtuber ไทย</h2>
+			<br />
+			<div className="text-3xl font-bold">
+				ทำไมถึงต้องมีผู้ว่า Youtuber ?
+			</div>
+			<div>
+			ผู้ว่า Youtuber จะเป็นเหมือนหน้าตาของ Youtuber ในไทย 
+			</div>
 		</div>
 	)
 };
--- client/src/pages/Trend.tsx
+++ client/src/pages/Trend.tsx
@@ -4,6 +4,7 @@ const Trend: React.FC = () => {
 	return(
 		<div className="text-center"> 
 			<h1 className="font-bold text-3xl">Trend</h1>
+			<h2 className="text-xl py-3">กระแส Youtuber ไทยวันนี้</h2>
 		</div>
 	)
 };
